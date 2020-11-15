90453e6ced52c50f34d387caec262d6e65bf23c4
Tom Irimescu <tom@irimescu.com>
2020-11-15 12:11:10 -0500

    Add body parser & admin, shop routes

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-// const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/favicon.ico', (req, res) => res.status(204));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs!');
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>The "Add Product" Page</h1>');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('In the middleware!');
-  res.send('<h1>Hello from Express!</h1>');
-});
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
