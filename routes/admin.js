01d4ddd8fc088ca1bdc8b7d37f992ba1117a19bb
Tom Irimescu <tom@irimescu.com>
2020-11-15 15:12:32 -0500

    Add 404, add-product, shop views, path utility

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,7 @@
+const path = require('path');
+
+const rootDir = require('../util/path');
+
 const express = require('express');
 
 const router = express.Router();
@@ -5,10 +9,10 @@ const router = express.Router();
 router.get('/favicon.ico', (req, res) => res.status(204));
 
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
