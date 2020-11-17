0da9df98f8d98acaf59259209bfa4da20fb03062
Tom Irimescu <tom@irimescu.com>
2020-11-16 19:49:44 -0500

    WIP refactor views to EJS templates

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,24 @@
-const path = require('path');
-
-const rootDir = require('../util/path');
-
 const express = require('express');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/favicon.ico', (req, res) => res.status(204));
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { 
+    pageTitle: 'Add Product',
+    path: '/add-product'
+  })
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
