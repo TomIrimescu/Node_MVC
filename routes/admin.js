cb584648f95890639bf5d087c3bd3c8e8404ac73
Tom Irimescu <tom@irimescu.com>
2020-11-17 17:49:39 -0500

    Refactor request/response into controllers

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,15 @@
 const express = require('express');
 
-const router = express.Router();
+const productsController = require('../controllers/products');
 
-const products = [];
+const router = express.Router();
 
 router.get('/favicon.ico', (req, res) => res.status(204));
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,21 +1,11 @@
-const adminData = require('./admin');
-
 const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
 router.get('/favicon.ico', (req, res) => res.status(204));
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', { 
-    prods: products, 
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true  
-  });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
