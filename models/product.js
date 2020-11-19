b33ea91f6695f0f3323bb03b2fe903e3ad97fae3
Tom Irimescu <tom@irimescu.com>
2020-11-19 13:42:24 -0500

    Add orders nav, view, controller, update admin products

--- models/product.js
+++ models/product.js
@@ -18,7 +18,7 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, price,description) {
+  constructor(title, imageUrl, price, description) {
     this.title = title;
     this.imageUrl = imageUrl;
     this.price = price;
