afa5e2346b58d2854d04da91c5b363dab8f14026
Tom Irimescu <tom@irimescu.com>
2020-11-22 13:36:18 -0500

    add comments

--- routes/shop.js
+++ routes/shop.js
@@ -6,18 +6,25 @@ const router = express.Router();
 
 router.get('/favicon.ico', (req, res) => res.status(204));
 
+// / => GET
 router.get('/', shopController.getIndex);
 
+// /products => GET
 router.get('/products', shopController.getProducts);
 
+// /products => GET
 router.get('/products/:productId', shopController.getProduct);
 
+// /cart => GET
 router.get('/cart', shopController.getCart);
 
+// /cart => POST
 router.post('/cart', shopController.postCart);
 
+// /orders => GET
 router.get('/orders', shopController.getOrders);
 
+// /checkout => GET
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
