35291d580ef640a622b7b141aa3e82aa4e640913
Tom Irimescu <tom@irimescu.com>
2020-12-08 15:35:44 -0500

    Create product schema, refactor postAddProduct

--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,14 @@ const router = express.Router();
 
 router.get('/favicon.ico', (req, res) => res.status(204));
 
-// // /admin/add-product => GET
-// router.get('/add-product', adminController.getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
 // // /admin/products => GET
 // router.get('/products', adminController.getProducts);
 
-// // /admin/add-product => POST
-// router.post('/add-product', adminController.postAddProduct);
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 // // /admin/edit-product => GET
 // router.get('/edit-product/:productId', adminController.getEditProduct);
