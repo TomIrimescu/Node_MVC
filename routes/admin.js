73b2183b2ff2499e3be5a63d05e53b25c22a4936
Tom Irimescu <tom@irimescu.com>
2020-12-08 15:04:27 -0500

    Install Mongoose, set-up connection, prep migration to ODM

--- routes/admin.js
+++ routes/admin.js
@@ -6,22 +6,22 @@ const router = express.Router();
 
 router.get('/favicon.ico', (req, res) => res.status(204));
 
-// /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+// // /admin/add-product => GET
+// router.get('/add-product', adminController.getAddProduct);
 
-// /admin/products => GET
-router.get('/products', adminController.getProducts);
+// // /admin/products => GET
+// router.get('/products', adminController.getProducts);
 
-// /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
+// // /admin/add-product => POST
+// router.post('/add-product', adminController.postAddProduct);
 
-// /admin/edit-product => GET
-router.get('/edit-product/:productId', adminController.getEditProduct);
+// // /admin/edit-product => GET
+// router.get('/edit-product/:productId', adminController.getEditProduct);
 
-// /admin/edit-product => POST
-router.post('/edit-product', adminController.postEditProduct);
+// // /admin/edit-product => POST
+// router.post('/edit-product', adminController.postEditProduct);
 
-// /admin/delete-product => POST
-router.post('/delete-product', adminController.postDeleteProduct);
+// // /admin/delete-product => POST
+// router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -6,28 +6,28 @@ const router = express.Router();
 
 router.get('/favicon.ico', (req, res) => res.status(204));
 
-// / => GET
-router.get('/', shopController.getIndex);
+// // / => GET
+// router.get('/', shopController.getIndex);
 
-// /products => GET
-router.get('/products', shopController.getProducts);
+// // /products => GET
+// router.get('/products', shopController.getProducts);
 
-// /products/12345 => GET
-router.get('/products/:productId', shopController.getProduct);
+// // /products/12345 => GET
+// router.get('/products/:productId', shopController.getProduct);
 
-// /cart => GET
-router.get('/cart', shopController.getCart);
+// // /cart => GET
+// router.get('/cart', shopController.getCart);
 
-// /cart => POST
-router.post('/cart', shopController.postCart);
+// // /cart => POST
+// router.post('/cart', shopController.postCart);
 
-// /cart-delete-item => POST
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+// // /cart-delete-item => POST
+// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-// /create-order
-router.post('/create-order', shopController.postOrder);
+// // /create-order
+// router.post('/create-order', shopController.postOrder);
 
-// /orders => GET
-router.get('/orders', shopController.getOrders);
+// // /orders => GET
+// router.get('/orders', shopController.getOrders);
 
 module.exports = router;
