1899c2c986df95cc2031e4c3e51ca54cf7a794bd
Tom Irimescu <tom@irimescu.com>
2021-01-12 13:56:50 -0500

    Add auth middleware for route protection

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -16,18 +17,18 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 
 // /cart => GET
-router.get('/cart', shopController.getCart);
+router.get('/cart', isAuth, shopController.getCart);
 
 // /cart => POST
-router.post('/cart', shopController.postCart);
+router.post('/cart', isAuth, shopController.postCart);
 
 // /cart-delete-item => POST
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
 // /create-order
-router.post('/create-order', shopController.postOrder);
+router.post('/create-order', isAuth, shopController.postOrder);
 
 // /orders => GET
-router.get('/orders', shopController.getOrders);
+router.get('/orders', isAuth, shopController.getOrders);
 
 module.exports = router;
