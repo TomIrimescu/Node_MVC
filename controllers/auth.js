8cf1f75bb95bdaf4fbc2a5ca428bd360bcb4b6a0
Tom Irimescu <tom@irimescu.com>
2021-01-15 15:44:19 -0500

    Add validation/custom validation on signup email

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,7 @@ const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 require('dotenv').config();
 const SEND_GRID_API = process.env.SEND_GRID_API;
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -77,6 +78,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
